eeb4af0f1d05d552708141fcb45ac9eda82524b1
Christian Müller <chmllr@me.com>
2021-05-15 17:46:30 +0200

    add reading of PEM from STDIN

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ mod util;
 #[derive(Clap)]
 #[clap(name("nano"), global_setting = AppSettings::ColoredHelp)]
 pub struct CliOpts {
-    /// Path to your PEM file
+    /// Path to your PEM file (use "-" for STDIN)
     #[clap(long)]
     pem_file: Option<String>,
 
@@ -23,9 +23,18 @@ pub struct CliOpts {
 fn main() {
     let opts = CliOpts::parse();
     let command = opts.command;
-    let pem = opts
-        .pem_file
-        .map(|path| std::fs::read_to_string(path).expect("Couldn't read PEM file"));
+    let pem = opts.pem_file.map(|path| match path.as_str() {
+        // read from STDIN
+        "-" => {
+            let mut buffer = String::new();
+            use std::io::Read;
+            std::io::stdin()
+                .read_to_string(&mut buffer)
+                .expect("Couldn't read from STDIN");
+            buffer
+        }
+        path => std::fs::read_to_string(path).expect("Couldn't read PEM file"),
+    });
     let env = EnvironmentImpl::new(pem).expect("Couldn't instantiate the environment");
     match commands::exec(&env, command) {
         Err(err) => {
