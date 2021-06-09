5dc2fac647b05022f8ebf35eb3a5f02ae0fae949
Christian Müller <christian@dfinity.org>
2021-06-09 14:40:11 +0200

    fixes broken pipe error

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -2,6 +2,7 @@
 
 use crate::lib::AnyhowResult;
 use clap::Clap;
+use std::io::{self, Write};
 use tokio::runtime::Runtime;
 
 mod neuron_manage;
@@ -28,24 +29,36 @@ pub fn exec(pem: &Option<String>, cmd: Command) -> AnyhowResult {
     let runtime = Runtime::new().expect("Unable to create a runtime");
     match cmd {
         Command::PublicIds => public::exec(pem),
-        Command::Transfer(opts) => runtime.block_on(async {
-            transfer::exec(pem, opts).await.and_then(|out| {
-                println!("{}", serde_json::to_string(&out)?);
-                Ok(())
-            })
-        }),
+        Command::Transfer(opts) => {
+            runtime.block_on(async { transfer::exec(pem, opts).await.and_then(|out| print(&out)) })
+        }
         Command::NeuronStake(opts) => runtime.block_on(async {
-            neuron_stake::exec(pem, opts).await.and_then(|out| {
-                println!("{}", serde_json::to_string(&out)?);
-                Ok(())
-            })
+            neuron_stake::exec(pem, opts)
+                .await
+                .and_then(|out| print(&out))
         }),
         Command::NeuronManage(opts) => runtime.block_on(async {
-            neuron_manage::exec(pem, opts).await.and_then(|out| {
-                println!("{}", serde_json::to_string(&out)?);
-                Ok(())
-            })
+            neuron_manage::exec(pem, opts)
+                .await
+                .and_then(|out| print(&out))
         }),
         Command::Send(opts) => runtime.block_on(async { send::exec(pem, opts).await }),
     }
+}
+
+// Using println! for printing to STDOUT and piping it to other tools leads to
+// the problem that when the other tool closes its stream, the println! macro
+// panics on the error and the whole binary crashes. This function provides a
+// graceful handling of the error.
+fn print<T>(arg: &T) -> AnyhowResult
+where
+    T: ?Sized + serde::ser::Serialize,
+{
+    if let Err(e) = io::stdout().write_all(serde_json::to_string(&arg)?.as_bytes()) {
+        if e.kind() != std::io::ErrorKind::BrokenPipe {
+            eprintln!("{}", e);
+            std::process::exit(1);
+        }
+    }
+    Ok(())
 }
