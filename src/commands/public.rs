4084fb831502afe98d128dca4c9e24e95a2a4953
John Plevyak <jplevyak@gmail.com>
2021-09-20 07:45:07 -0700

    Add an option to public-ids to pass the principal. (#53)
    * Add an option to public-ids to pass the principal.
    This can be used to verify the account_id of a principal.
    
    Signed-off-by: John Plevyak <jplevyak@gmail.com>
    
    * Update version and tests.
    
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- src/commands/public.rs
+++ src/commands/public.rs
@@ -1,16 +1,38 @@
 use crate::lib::{get_identity, AnyhowResult};
 use anyhow::anyhow;
+use clap::Clap;
 use ic_base_types::PrincipalId;
 use ic_types::principal::Principal;
 use ledger_canister::AccountIdentifier;
 use std::convert::TryFrom;
 
+#[derive(Clap)]
+pub struct PublicOpts {
+    // Principal for which to get the account_id.
+    #[clap(long)]
+    principal_id: Option<String>,
+}
+
 /// Prints the account and the principal ids.
-pub fn exec(pem: &Option<String>) -> AnyhowResult {
-    let (principal_id, account_id) = get_ids(pem)?;
+pub fn exec(pem: &Option<String>, opts: PublicOpts) -> AnyhowResult {
+    let (principal_id, account_id) = get_public_ids(pem, opts)?;
     println!("Principal id: {}", principal_id.to_text());
     println!("Account id: {}", account_id);
     Ok(())
+}
+
+/// Returns the account id and the principal id if the private key was provided.
+fn get_public_ids(
+    pem: &Option<String>,
+    opts: PublicOpts,
+) -> AnyhowResult<(Principal, AccountIdentifier)> {
+    match opts.principal_id {
+        Some(principal_id) => {
+            let principal_id = ic_types::Principal::from_text(principal_id)?;
+            Ok((principal_id, get_account_id(principal_id)?))
+        }
+        None => get_ids(pem),
+    }
 }
 
 /// Returns the account id and the principal id if the private key was provided.
@@ -21,8 +43,11 @@ pub fn get_ids(pem: &Option<String>) -> AnyhowResult<(Principal, AccountIdentifi
     )
     .sender()
     .map_err(|e| anyhow!(e))?;
+    Ok((principal_id, get_account_id(principal_id)?))
+}
+
+fn get_account_id(principal_id: Principal) -> AnyhowResult<AccountIdentifier> {
     let base_types_principal =
         PrincipalId::try_from(principal_id.as_slice()).map_err(|err| anyhow!(err))?;
-    let account_id = AccountIdentifier::new(base_types_principal, None);
-    Ok((principal_id, account_id))
+    Ok(AccountIdentifier::new(base_types_principal, None))
 }
