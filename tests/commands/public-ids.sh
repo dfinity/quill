63f08b0d0584a6a59a8c47550779be9373365268
John Wiegley <johnw@newartisans.com>
2021-10-03 09:39:37 -0700

    Generalize tests to work in more environments (#71)

--- tests/commands/public-ids.sh
+++ tests/commands/public-ids.sh
@@ -1,2 +1,2 @@
-../target/debug/quill --pem-file - public-ids
-../target/debug/quill public-ids --principal-id 44mwt-bq3um-tqicz-bwhad-iipx4-6wzex-olvaj-z63bj-wkelv-xoua3-rqe
+${CARGO_TARGET_DIR:-../target}/debug/quill --pem-file - public-ids
+${CARGO_TARGET_DIR:-../target}/debug/quill public-ids --principal-id 44mwt-bq3um-tqicz-bwhad-iipx4-6wzex-olvaj-z63bj-wkelv-xoua3-rqe
