e561013a19ec42f4a55aa3670135d85a904ed951
Christian Müller <christian@dfinity.org>
2021-11-18 09:53:53 +0100

    use signing of status request from rs-agent

--- src/lib/signing.rs
+++ src/lib/signing.rs
@@ -1,20 +1,17 @@
 use crate::lib::get_idl_string;
 use crate::lib::AnyhowResult;
-use crate::lib::{get_candid_type, get_identity, get_local_candid};
+use crate::lib::{get_candid_type, get_local_candid};
 use anyhow::anyhow;
 use ic_agent::agent::QueryBuilder;
 use ic_agent::agent::UpdateBuilder;
-use ic_agent::{to_request_id, AgentError, Identity, RequestId};
-use ic_types::{hash_tree::Label, principal::Principal};
+use ic_agent::RequestId;
+use ic_types::principal::Principal;
 use serde::{Deserialize, Serialize};
 use serde_cbor::Value;
 use std::convert::TryFrom;
 use std::time::Duration;
-use std::time::SystemTime;
 
 use super::get_agent;
-
-const IC_REQUEST_DOMAIN_SEPARATOR: &[u8; 11] = b"\x0Aic-request";
 
 #[derive(Debug)]
 pub struct MessageError(String);
@@ -88,87 +85,17 @@ impl Ingress {
     }
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
-#[serde(tag = "request_type")]
-pub enum CallRequestContent {
-    #[serde(rename = "call")]
-    CallRequest {
-        ingress_expiry: u64,
-        sender: Principal,
-        canister_id: Principal,
-        method_name: String,
-        #[serde(with = "serde_bytes")]
-        arg: Vec<u8>,
-    },
-}
-
-#[derive(Debug, Clone, Deserialize, Serialize)]
-#[serde(tag = "request_type")]
-pub enum QueryContent {
-    #[serde(rename = "query")]
-    QueryRequest {
-        ingress_expiry: u64,
-        sender: Principal,
-        canister_id: Principal,
-        method_name: String,
-        #[serde(with = "serde_bytes")]
-        arg: Vec<u8>,
-    },
-}
-
-#[derive(Debug, Clone, Serialize)]
-#[serde(rename_all = "snake_case")]
-pub struct Envelope<T: Serialize> {
-    pub content: T,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(with = "serde_bytes")]
-    pub sender_pubkey: Option<Vec<u8>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[serde(with = "serde_bytes")]
-    pub sender_sig: Option<Vec<u8>>,
-}
-
-#[derive(Debug, Clone, Deserialize, Serialize)]
-#[serde(tag = "request_type")]
-pub enum ReadStateContent {
-    #[serde(rename = "read_state")]
-    ReadStateRequest {
-        ingress_expiry: u64,
-        sender: Principal,
-        paths: Vec<Vec<Label>>,
-    },
-}
-
-fn get_expiry_timestamp() -> AnyhowResult<u64> {
-    let timeout = std::time::Duration::from_secs(5 * 60);
-    Ok(SystemTime::now()
-        .checked_add(timeout)
-        .ok_or_else(|| anyhow!("Time wrapped around."))?
-        .duration_since(std::time::UNIX_EPOCH)
-        .expect("Time wrapped around")
-        .as_nanos() as u64)
-}
-
 pub fn request_status_sign(
     pem: &str,
     request_id: RequestId,
     canister_id: Principal,
 ) -> AnyhowResult<RequestStatus> {
-    let paths: Vec<Vec<Label>> = vec![vec!["request_status".into(), request_id.as_slice().into()]];
-
-    let identity = get_identity(pem);
-    let ingress_expiry = get_expiry_timestamp()?;
-
-    let req = ReadStateContent::ReadStateRequest {
-        sender: identity.sender().map_err(AgentError::SigningError)?,
-        paths,
-        ingress_expiry,
-    };
-    let (bytes, _) = sign_content(identity, req)?;
+    let agent = get_agent(pem)?;
+    let val = agent.sign_request_status(canister_id, request_id)?;
     Ok(RequestStatus {
         canister_id: canister_id.to_string(),
         request_id: request_id.into(),
-        content: hex::encode(bytes),
+        content: hex::encode(val.signed_request_status),
     })
 }
 
@@ -215,33 +142,6 @@ pub fn sign(
         },
         request_id,
     })
-}
-
-fn sign_content<'a, T>(
-    identity: Box<dyn Identity + Send + Sync>,
-    request: T,
-) -> Result<(Vec<u8>, RequestId), AgentError>
-where
-    T: 'a + Serialize,
-{
-    let request_id = to_request_id(&request)?;
-    let mut msg = vec![];
-    msg.extend_from_slice(IC_REQUEST_DOMAIN_SEPARATOR);
-    msg.extend_from_slice(request_id.as_slice());
-    let signature = identity.sign(&msg).map_err(AgentError::SigningError)?;
-
-    let envelope = Envelope {
-        content: request,
-        sender_pubkey: signature.public_key,
-        sender_sig: signature.signature,
-    };
-
-    let mut serialized_bytes = Vec::new();
-    let mut serializer = serde_cbor::Serializer::new(&mut serialized_bytes);
-    serializer.self_describe()?;
-    envelope.serialize(&mut serializer)?;
-
-    Ok((serialized_bytes, request_id))
 }
 
 /// Generates a bundle of signed messages (ingress + request status query).
