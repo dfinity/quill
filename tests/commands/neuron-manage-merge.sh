7dcb282fbde0160d92dd6b0b274aa7a663e4e0d5
John Wiegley <johnw@newartisans.com>
2021-12-08 15:32:37 -0800

    Fix a test

--- tests/commands/neuron-manage-merge.sh
+++ tests/commands/neuron-manage-merge.sh
@@ -1,1 +1,1 @@
-${CARGO_TARGET_DIR:-../target}/debug/quill --pem-file - neuron-manage 2313380519530470538 --merge-neuron 2313380519530470538 | ${CARGO_TARGET_DIR:-../target}/debug/quill send --dry-run -
+${CARGO_TARGET_DIR:-../target}/debug/quill --pem-file - neuron-manage 2313380519530470538 --merge-from-neuron 2313380519530470538 | ${CARGO_TARGET_DIR:-../target}/debug/quill send --dry-run -
