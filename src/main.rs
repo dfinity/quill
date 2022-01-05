9fc4d184f4f520be32dee7614ff27d9c26abec23
John Wiegley <johnw@newartisans.com>
2022-01-05 11:46:57 -0800

    Port changes from last merge

--- src/main.rs
+++ src/main.rs
@@ -36,17 +36,36 @@ fn main() {
     let opts = CliOpts::parse();
     let command = opts.command;
     // Get PEM from the file if provided, or try to convert from the seed file
-    let pem = match opts.pem_file {
-        Some(path) => Some(read_file(path)),
-        None => opts.seed_file.map(|path| {
-            let phrase = read_file(path);
-            lib::mnemonic_to_pem(
-                &Mnemonic::parse(phrase)
-                    .expect("Couldn't parse the seed phrase as a valid mnemonic"),
-            )
-        }),
+    let auth = if opts.hsm {
+        let mut hsm = lib::HSMInfo::new();
+        if let Some(path) = opts.hsm_libpath {
+            hsm.libpath = std::path::PathBuf::from(path);
+        }
+        if let Some(slot) = opts.hsm_slot {
+            hsm.slot = slot;
+        }
+        if let Some(id) = opts.hsm_id {
+            hsm.ident = id;
+        }
+        lib::AuthInfo::NitroHsm(hsm)
+    } else {
+        let pem = match opts.pem_file {
+            Some(path) => Some(read_file(path)),
+            None => opts.seed_file.map(|path| {
+                let phrase = read_file(path);
+                lib::mnemonic_to_pem(
+                    &Mnemonic::parse(phrase)
+                        .expect("Couldn't parse the seed phrase as a valid mnemonic"),
+                )
+            }),
+        };
+        if let Some(pem) = pem {
+            lib::AuthInfo::PemFile(pem)
+        } else {
+            lib::AuthInfo::NoAuth
+        }
     };
-    if let Err(err) = commands::exec(&pem, command) {
+    if let Err(err) = commands::exec(&auth, command) {
         eprintln!("{}", err);
         std::process::exit(1);
     }
@@ -68,35 +87,5 @@ fn read_file(path: String) -> String {
             eprintln!("Couldn't read PEM file: {:?}", err);
             std::process::exit(1);
         }),
-<<<<<<< A: HEAD
-    });
-    let auth = if opts.hsm {
-        let mut hsm = lib::HSMInfo::new();
-        if let Some(path) = opts.hsm_libpath {
-            hsm.libpath = std::path::PathBuf::from(path);
-        }
-        if let Some(slot) = opts.hsm_slot {
-            hsm.slot = slot;
-        }
-        if let Some(id) = opts.hsm_id {
-            hsm.ident = id;
-        }
-        lib::AuthInfo::NitroHsm(hsm)
-    } else {
-        match pem {
-            Some(path) => lib::AuthInfo::PemFile(path),
-            None => lib::AuthInfo::NoAuth,
-        }
-    };
-    if let Err(err) = commands::exec(&auth, command) {
-        eprintln!("{}", err);
-        std::process::exit(1);
-||||||| Ancestor
-    });
-    if let Err(err) = commands::exec(&pem, command) {
-        eprintln!("{}", err);
-        std::process::exit(1);
-=======
->>>>>>> B: Incoming
     }
 }
