c57c5ec1a7cecb6dc65b3b1f34092f422c1b1e56
Adam Spofford <adam.spofford@dfinity.org>
2022-08-01 18:30:23 -0700

    carefully hidden test

--- e2e/tests-quill/create_neuron.bash
+++ e2e/tests-quill/create_neuron.bash
@@ -10,13 +10,13 @@ teardown() {
 
 @test "basic create neuron" {
     #account is initialized with 10_000 tokens
-    assert_command quill --insecure-local-dev-mode account-balance 345f723e9e619934daac6ae0f4be13a7b0ba57d6a608e511a00fd0ded5866752 --yes
+    assert_command quill account-balance 345f723e9e619934daac6ae0f4be13a7b0ba57d6a608e511a00fd0ded5866752 --yes --insecure-local-dev-mode
     assert_string_match '(record { e8s = 1_000_000_000_000 : nat64 })'
 
     # stake 3 tokens
-    assert_command bash -c "quill --pem-file $PEM_LOCATION/identity.pem neuron-stake --amount 3 --name myneur > stake.call"
+    assert_command bash -c "quill neuron-stake --amount 3 --name myneur --pem-file $PEM_LOCATION/identity.pem > stake.call"
     assert_file_not_empty stake.call
-    SEND_OUTPUT="$(quill --insecure-local-dev-mode send stake.call --yes)"
+    SEND_OUTPUT="$(quill send stake.call --yes --insecure-local-dev-mode)"
     assert_command echo "$SEND_OUTPUT" # replay the output so string matches work
     echo "$SEND_OUTPUT"
     assert_string_match "Method name: claim_or_refresh_neuron_from_account"
@@ -25,16 +25,16 @@ teardown() {
     assert_string_match "record { result = opt variant { NeuronId = record { id =" #fragment of a correct response
 
     # check that staking worked using get-neuron-info
-    assert_command bash -c "quill --insecure-local-dev-mode get-neuron-info $NEURON_ID --yes"
+    assert_command bash -c "quill get-neuron-info $NEURON_ID --yes --insecure-local-dev-mode"
     assert_string_match 'stake_e8s = 300_000_000'
 
     # increase dissolve delay by 6 months
-    assert_command bash -c "quill --pem-file $PEM_LOCATION/identity.pem neuron-manage --additional-dissolve-delay-seconds 15778800 $NEURON_ID > more-delay.call"
+    assert_command bash -c "quill neuron-manage --additional-dissolve-delay-seconds 15778800 --pem-file $PEM_LOCATION/identity.pem $NEURON_ID > more-delay.call"
     assert_file_not_empty more-delay.call
-    assert_command quill --insecure-local-dev-mode send more-delay.call --yes #provides no interesting output on succes. Command not failing is good enough here
+    assert_command quill send more-delay.call --yes --insecure-local-dev-mode #provides no interesting output on succes. Command not failing is good enough here
 
     # check that increasing dissolve delay worked, this time using list-neurons
-    assert_command bash -c "quill --pem-file $PEM_LOCATION/identity.pem list-neurons > neuron.call"
-    assert_command quill --insecure-local-dev-mode send neuron.call --yes
+    assert_command bash -c "quill list-neurons --pem-file $PEM_LOCATION/identity.pem > neuron.call"
+    assert_command quill send neuron.call --yes --insecure-local-dev-mode
     assert_string_match "dissolve_delay_seconds = 15_778_800"
 }
