539f6b1dc7944e0483bd23670d710b4ab443d4f2
Adam Spofford <adam.spofford@dfinity.org>
2023-01-27 18:56:58 -0800

    needs 1.66

--- scripts/workflows/e2e-matrix.py
+++ scripts/workflows/e2e-matrix.py
@@ -17,7 +17,7 @@ test = sorted(test_scripts('quill'))
 matrix = {
     'test': test,
     'os': [ 'macos-latest', 'ubuntu-latest' ],
-    'rust': [ '1.65.0' ],
+    'rust': [ '1.66.1' ],
     'exclude': [ ]
 }
 
