ae523124a8181073ccbaca16b7d35fd6fbfb0d53
Linwei Shang <linwei.shang@dfinity.org>
2023-02-08 22:03:48 -0500

    fix test

--- tests/commands/sns-pay.sh
+++ tests/commands/sns-pay.sh
@@ -1,1 +1,1 @@
-"$QUILL" sns pay --amount-icp-e8s 100000 --subaccount e000d80101 --ticket-creation-time 1675815621 --ticket-id 100 --canister-ids-file ./sns_canister_ids.json --pem-file - | "$QUILL" send --dry-run -
+"$QUILL" sns pay --amount-icp-e8s 100000 --subaccount e000d80101 --ticket-id 100 --canister-ids-file ./sns_canister_ids.json --pem-file - | "$QUILL" send --dry-run -
